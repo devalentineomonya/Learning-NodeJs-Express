32d2ef51aac1d8fd6ea7481d422c4c872fd4175a
Valentine Omonya <valomosh254@gmail.com>
2024-05-19 18:04:35 +0300

    Initialized Calculator project

--- 1.0NodeBeginning/server.js
+++ 1.0NodeBeginning/server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Hello world!");
+    res.send("<h1>Hello world!</h1>");
 })
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("Server running on port 3000");
-})
+})  
