f63f40857c0a29ff248128b84f7fb9cebb692042
Valentine Omonya <valomosh254@gmail.com>
2024-05-28 21:53:44 +0300

    Return a list of customers

--- 2.0NodeExpressServer/servertwo.js
+++ 2.0NodeExpressServer/servertwo.js
@@ -1,10 +1,42 @@
-const app = require('express')();
+const { urlencoded } = require('express');
 
-app.get("/",(req, res) => {
-    console.log("Loogging");  
+const app = require('express')();
+const customersList = [
+
+    {
+        id: '1',
+        name: 'Customer1',
+        city: 'San Francisco',
+    },
+    {
+        id: '2',
+        name: 'Customer2',
+        city: 'London'
+    },
+    {
+        id: '3',
+        name: 'Customer3',
+        city: 'Nairobi'
+    },
+    {
+        id: '4',
+        name: 'Customer4',
+        city: 'Paris'
+    }
+]
+
+app.get("/", (req, res) => {
+    console.log("Loogging");
 })
+
+
+app.get("/customer", (req, res) => {
+    res.json(customersList)
+})
+
+
 const port = process.env.PORT_NUMBER || 3000
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Node App running on port ${port}`);
 })     
